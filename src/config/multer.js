584c968ad83aa40a72c19fde038d164f3aba0da6
Iann Santos <iannsantos8@gmail.com>
2020-03-21 21:24:45 -0300

    feat: add storage s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,9 +1,13 @@
+import aws from "aws-sdk";
 import crypto from "crypto";
 import multer from "multer";
+import multers3 from "multer-s3";
 import { extname, resolve } from "path";
 
-export default {
-  storage: multer.diskStorage({
+const { BUCKET_NAME, STORAGE_TYPE } = process.env;
+
+const storageTypes = {
+  local: multer.diskStorage({
     destination: resolve(__dirname, "..", "..", "tmp", "uploads"),
     filename: (req, file, callback) => {
       crypto.randomBytes(15, (err, res) => {
@@ -16,5 +20,41 @@ export default {
         return callback(null, res.toString("hex") + extname(file.originalname));
       });
     }
+  }),
+  s3: multers3({
+    s3: new aws.S3(),
+    bucket: BUCKET_NAME,
+    contentType: multers3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, res, callback) => {
+      crypto.randomBytes(15, (err, res) => {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, res.toString("hex") + extname(file.originalname));
+      });
+    }
   })
+};
+
+export default {
+  dest: resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes[STORAGE_TYPE],
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  },
+  fileFilter: (req, file, callback) => {
+    const allowedMimes = [
+      "image/jpeg",
+      "image/pjpeg",
+      "image/png",
+      "image/gif"
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Invalid file type"));
+    }
+  }
 };
