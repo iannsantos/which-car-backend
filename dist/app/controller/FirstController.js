68ea3185783012776a58bc43d0623621f52f88d8
Iann Santos <iannsantos8@gmail.com>
2020-03-21 21:26:30 -0300

    chore: built prod files

--- dist/app/controller/FirstController.js
+++ dist/app/controller/FirstController.js
@@ -2,23 +2,32 @@
 
 class FirstController {
   async show(req, res) {
+    const { APP_URL, STORAGE_TYPE, STORAGE_URL } = process.env;
     try {
       const { filename } = req.file;
 
-      const input = image => `http://localhost:3333/files/${image}`;
+      let input;
+
+      if (STORAGE_TYPE === "local") {
+        input = image => `${APP_URL}/files/${image}`;
+      } else if (STORAGE_TYPE === "s3") {
+        input = image => `${STORAGE_URL}/${image}`;
+      }
 
       const response = await _algorithmia2.default.call(void 0, input(filename));
 
-      const bestConfidence = response.result.find(
-        item => item.confidence === "1.00"
-      );
+      if (response.result.length >= 1) {
+        const bestConfidence = response.result.find(
+          item => item.confidence === "1.00"
+        );
 
-      return res.json(bestConfidence);
+        return res.json(bestConfidence);
+      } else {
+        return res.json({ error: true, message: "Not found car" });
+      }
     } catch (err) {
       return res.json({ error: true, message: err.message });
     }
-    // const input =
-    //   "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Geneva_International_Motor_Show_2018%2C_Le_Grand-Saconnex_%281X7A0083%29.jpg/1080px-Geneva_International_Motor_Show_2018%2C_Le_Grand-Saconnex_%281X7A0083%29.jpg";
   }
 }
 
--- dist/config/multer.js
+++ dist/config/multer.js
@@ -1,9 +1,13 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _crypto = require('crypto'); var _crypto2 = _interopRequireDefault(_crypto);
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _awssdk = require('aws-sdk'); var _awssdk2 = _interopRequireDefault(_awssdk);
+var _crypto = require('crypto'); var _crypto2 = _interopRequireDefault(_crypto);
 var _multer = require('multer'); var _multer2 = _interopRequireDefault(_multer);
+var _multers3 = require('multer-s3'); var _multers32 = _interopRequireDefault(_multers3);
 var _path = require('path');
 
-exports. default = {
-  storage: _multer2.default.diskStorage({
+const { BUCKET_NAME, STORAGE_TYPE } = process.env;
+
+const storageTypes = {
+  local: _multer2.default.diskStorage({
     destination: _path.resolve.call(void 0, __dirname, "..", "..", "tmp", "uploads"),
     filename: (req, file, callback) => {
       _crypto2.default.randomBytes(15, (err, res) => {
@@ -16,5 +20,41 @@ exports. default = {
         return callback(null, res.toString("hex") + _path.extname.call(void 0, file.originalname));
       });
     }
+  }),
+  s3: _multers32.default.call(void 0, {
+    s3: new _awssdk2.default.S3(),
+    bucket: BUCKET_NAME,
+    contentType: _multers32.default.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, res, callback) => {
+      _crypto2.default.randomBytes(15, (err, res) => {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, res.toString("hex") + _path.extname.call(void 0, file.originalname));
+      });
+    }
   })
+};
+
+exports. default = {
+  dest: _path.resolve.call(void 0, __dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes[STORAGE_TYPE],
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  },
+  fileFilter: (req, file, callback) => {
+    const allowedMimes = [
+      "image/jpeg",
+      "image/pjpeg",
+      "image/png",
+      "image/gif"
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Invalid file type"));
+    }
+  }
 };
